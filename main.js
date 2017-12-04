a7af36450c3a37f6e90bd6448d3194ce23fa51e9
Michael Smith <mjsmitths@gmail.com>
2017-12-04 17:37:08 -0500

    commit

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ jQuery.fn.loadRepositories = function(username) {
    
         var list = $('<dl/>');
         target.empty().append(list);
-        var newRepos = $(repos).slice(0,6);
+        var newRepos = $(repos).slice(0,5);
         $(newRepos).each(function() {
             if (this.name != (username.toLowerCase()+'.github.com')) {
-                var myDate = new Date(this.updated_at);
+                var repoDate = new Date(this.updated_at);
                 list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'"><strong>' + this.name + '</strong></a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
                 list.append('<dd>' + this.description +'</dd>');
-                list.append("<em>Updated at: " + '<dd>' + (myDate.getMonth() + 1) + '/'+ myDate.getDate() + '/' + myDate.getFullYear()  + '</dd></em>');
+                list.append("<em>Updated at: " + '<dd>' + (repoDate.getMonth() + 1) + '/'+ repoDate.getDate() + '/' + repoDate.getFullYear()  + '</dd></em>');
             }
         });      
      });
@@ -29,19 +29,5 @@ jQuery.fn.loadRepositories = function(username) {
     b = new Date(b.updated_at);
     return a>b ? -1 : a<b ? 1 : 0;
   });
-    }
-    function submit() {
-    alert("Our server is currently down.  Please email angelarowe7@gmail.com directly.");
-}
+ }
 };
- 
-      
- /*  function sortByName(repos) {
-        repos.sort(function(a,b) {
-            if(a.name < b.name) {
-                return -1;
-            }
-        return a.name - b.name;
-       });
-    }
-}; */
